75365c8a5894b8c8c87d65d5cbfafa48faf13aa8
Hanjin <32667737+hanzyn09@users.noreply.github.com>
2021-03-24 23:48:26 +0900

    HashMap summary
    HashMap summary

---
+++ chap13-collection-framework/src/com/hanzyn/section04/map/HashMapEx.java
@@ -0,0 +1,21 @@
+package com.hanzyn.section04.map;
+
+public class HashMapEx {
+	/* [Map]
+	 * : 키(Key)와 값(Value) 쌍으로 저장
+	 * 주요 클래스
+	 * : HashMap, Hashtable, TreeMap, Properties, LinkedHashMap
+	 * 
+	 * key 와 value는 모두 객체로 이루어지고,
+	 * key는 중복이 불가능하고, value는 중복저장이 가능하다.
+	 * Map은 데이터를 찾아갈 수 있는 키와 실제 데이터인 밸류를 저장해서 Dictionary라고도 한다.
+	 * 
+	 * 해쉬 맵 : 키의 순서를 알 수 없다.
+	 * 트리 맵 : 키의 순서가 크기 순이다.
+	 * 링크드해쉬맵 : 키의 순서가 저장된 순서이다.
+	 * 
+	 * 저장하고 싶은 데이터 값이 value, 이 값을 가져올 때 필요한 것이 key 이다.
+	 * 동일한 키에 데이터를 두번이상 저장하면 두번째 부터는 데이터가 업데이트 된다.
+	 * 
+	 * */
+}
