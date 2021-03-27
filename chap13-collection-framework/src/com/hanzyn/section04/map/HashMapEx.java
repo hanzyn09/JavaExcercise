5e6702d2b0a7e36c327692197242f5b26eceabd9
Hanjin <32667737+hanzyn09@users.noreply.github.com>
2021-03-27 23:19:05 +0900

    Update HashMapEx.java
    get value from key : map.get()

--- chap13-collection-framework/src/com/hanzyn/section04/map/HashMapEx.java
+++ chap13-collection-framework/src/com/hanzyn/section04/map/HashMapEx.java
@@ -1,5 +1,8 @@
 package com.hanzyn.section04.map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapEx {
 	/* [Map]
 	 * : 키(Key)와 값(Value) 쌍으로 저장
@@ -18,4 +21,25 @@ public class HashMapEx {
 	 * 동일한 키에 데이터를 두번이상 저장하면 두번째 부터는 데이터가 업데이트 된다.
 	 * 
 	 * */
+	
+	public static void main(String[] args) {
+		//맵 객체 생성
+		Map map = new HashMap();
+		
+		//이름이 담긴 문자열 배열 생성
+		String[] names = {"홍길동", "김유신", "이순신", "강감찬", "김유신"};
+		//숫자가 담긴 정수 배열 생성
+		int[] nums = {1234, 5678, 2350, 9870, 2345};
+		
+		//map 객체에 두 배열의 값들을 키와 밸류 쌍으로 저장
+		for(int i=0; i<names.length; i++) {
+			map.put(names[i], nums[i]);
+		}
+		
+		//출력
+		System.out.println(map);
+		System.out.println("홍길동 번호 :  " + map.get("홍길동")); //map.get("") : key 값으로 저장된 객체를 검색하는 메서드.
+		System.out.println("이순신 번호 :  " + map.get("이순신")); // "~" 가 key 인 value를 리턴함.
+		System.out.println("김유신 번호 :  " + map.get("김유신")); //중복되어서 한개만 저장되므로 새로운 값으로 대체 된다.
+	}
 }
