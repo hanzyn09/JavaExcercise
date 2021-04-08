94d6550286e6a48d2d02ccb500d76965a2f44e7d
Hanjin <32667737+hanzyn09@users.noreply.github.com>
2021-04-08 23:08:21 +0900

    synchronized
    synchronized

---
+++ chap17-thread/src/com/hanzyn/section03/synchronize/ThreadEx10.java
@@ -0,0 +1,18 @@
+package com.hanzyn.section03.synchronize;
+
+public class ThreadEx10 {
+	public static void main(String[] args) {
+		//게임 객체 생성
+		SmartPhoneGame game = new SmartPhoneGame();
+	}
+}
+
+class SmartPhoneGame{
+	private int level; //level
+	
+	public int getLevel() {
+		return this.level;
+	}
+	
+	
+}
